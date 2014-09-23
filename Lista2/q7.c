3cdac40b096a770b30d7a5b261586e7cc3a41987
Joalisson de Andrade Silva <joalisson.as@gmail.com>
2014-09-23 08:41:53 -0300

    nova q

---
+++ Lista2/q7.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+
+void strString(char *strcopy(char *str)){
+     printf("String: %s", strString); 
+} 
+int main(){ 
+    printf("A copia é %s", strString);
+    return 0;
+    } 
+     
